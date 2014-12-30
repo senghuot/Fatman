1ec93c5530b1ea27835abafba9176cf3869c7d41
taloto07 <taloto07@gmail.com>
2014-12-29 23:23:15 -0800

    validate post page on client side

--- public/js/angular/post.js
+++ public/js/angular/post.js
@@ -4,25 +4,60 @@
 	app.controller('FormController', ['$http', function($http){
 		var myApp = this;
 
+		this.invalid = false;
+
 		this.categories = [];
 
 		this.subCategories = [];
 
-		this.photos = [0];
-		this.photoIndex = 1;
-		this.photoLeft = 11;
-
-		this.addPhoto = function(){
-			if (this.photoLeft > 0){
-				this.photos.push(this.photoIndex++);
-				this.photoLeft--;
-			}
-		}
+		this.photos = [];
+		this.photosGT12 = false;
+		this.photosNeed = false;
 
 		this.changeCateg = function(){
 			
 			this.subCategories = this.selectedCateg.sub_categories;
 			
+		}
+
+		this.fileSize = function(){
+			if (typeof FileReader !== 'undefined'){
+				var files = document.getElementById('photos').files;
+				for (var i = 0; i < files.length; i++){
+					alert(files[i].name);
+				}
+			}
+		}
+
+		this.submit = function(event){
+			this.invalid = true;
+
+			if (typeof FileReader !== 'undefined'){
+				this.photos = [];
+				var files = document.getElementById('photos').files;
+				
+				if (files.length === 0){
+					this.photosNeed = true;
+					this.photosGT12 = false;
+					event.preventDefault();
+				}else if (files.length > 12){
+					this.photosGT12 = true;
+					this.photosNeed= false;
+					event.preventDefault();
+				}else{
+					this.photosNeed = false;
+					this.photosGT12 = false;
+					for (var i = 0; i < files.length; i++){
+						if (files[i].size > 1*1024*1024*5){
+							this.photos.push(files[i]);
+						}
+							
+					}
+
+					if (this.photos.length > 0)
+						event.preventDefault();
+				}
+			}
 		}
 
 		//load category and sub_category from database
