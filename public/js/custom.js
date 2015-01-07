e72fab8fbbae99b13d709d18a309102c1b84b611
taloto07 <taloto07@gmail.com>
2015-01-06 22:45:03 -0800

    prettify search page

--- public/js/custom.js
+++ public/js/custom.js
@@ -22,8 +22,18 @@
     }
   });
 
+  //convert string to uppercase first letter. ex: "hello world" => "Hello World"
+  String.prototype.capitalize = function(){
+    return this.replace( /(^|\s)([a-z])/g , 
+      function(m,p1,p2){ 
+        return p1+p2.toUpperCase();
+      } );
+  };
+
   var skip = 1;
-  
+  var loadStatus = true;
+  var allowInfiniteScroll= false;
+
   // search button
   $('select#selectCategory').change(function(){
     var category = $('select#selectCategory');
@@ -45,6 +55,8 @@
 
   // click more button to more content
   $("button#more").click(function(){
+    // allowInfiniteScroll = true;
+    // $(this).hide();
     var myForm = $('form#search');
     var location = $("select#location");
     var keyword = $("input#keyword");
@@ -59,7 +71,7 @@
     data.skip = skip++;
 
     var request = $.ajax({
-      url: "http://localhost:3000/api/v1/search",
+      url: "/api/v1/search",
       type: "GET",
       dataType: "JSON",
       data: data,
@@ -78,8 +90,8 @@
           result += "</a>";
           result += "<div class='title'>" + data[i].title + "</div>";
           result += "<div class='price'>$" + data[i].price + "</div>";
-          result += "<div class='location'>" + data[i].location.city + "</div>";
-          result += "<div class='user'>" + data[i].user.fname + "</div>";
+          result += "<div class='location'>" + data[i].location.city.capitalize() + "</div>";
+          result += "<div class='user'>" + data[i].user.fname.capitalize() + "</div>";
         result += "</div>";
         result += "</div>";
 
@@ -95,5 +107,77 @@
       alert("Request failed: " + textStatus);
     });
   });
+  
+  // // load more content when scroll to bottom page
+  // function lastAddedLiveFunc() {
+  //   $('div#lastPostsLoader').html('loading');
+
+  //   var myForm = $('form#search');
+  //   var location = $("select#location");
+  //   var keyword = $("input#keyword");
+  //   var category = $("input#category");
+  //   var sort = $("input#sort");
+
+  //   var data = {};
+  //   data.location = location.val();
+  //   data.keyword = keyword.val();
+  //   data.category = category.val();
+  //   data.sort = sort.val();
+  //   data.skip = skip++;
+
+  //   var request = $.ajax({
+  //     url: "http://localhost:3000/api/v1/search",
+  //     type: "GET",
+  //     dataType: "JSON",
+  //     data: data,
+  //     contentType: "application/json; charset=UTF-8"
+  //   });
+
+  //   request.done(function(data){
+  //     var divResult = $("div#container");
+  //     console.log(data);
+
+  //     if (data == '') loadStatus = false;
+
+  //     for (var i = 0; i < data.length; i++){
+  //       var result = "<div class='post'>";
+  //       result += "<div class='post-wrapper'>";
+  //         result += "<a href='/display/" + data[i]._id + "'>";
+  //           result += "<img src='" + data[i].pictures[0] + "' />";
+  //         result += "</a>";
+  //         result += "<div class='title'>" + data[i].title + "</div>";
+  //         result += "<div class='price'>$" + data[i].price + "</div>";
+  //         result += "<div class='location'>" + data[i].location.city + "</div>";
+  //         result += "<div class='user'>" + data[i].user.fname + "</div>";
+  //       result += "</div>";
+  //       result += "</div>";
+
+
+  //       divResult.append(result);
+  //       divResult.masonry().imagesLoaded(function(){
+  //         divResult.masonry("reloadItems").masonry("layout");
+  //       });
+  //     }
+
+  //     $('div#lastPostsLoader').empty();
+  //   });
+
+  //   request.fail(function(jqXHR, textStatus){
+  //     alert("Request failed: " + textStatus);
+  //   });
+  // };
+
+  // //lastAddedLiveFunc();
+  // $(window).scroll(function(){
+
+  //     var wintop = $(window).scrollTop(), docheight = $(document).height(), winheight = $(window).height();
+  //     var  scrolltrigger = 0.5;
+
+  //     if  ((wintop/(docheight-winheight)) > scrolltrigger && loadStatus && allowInfiniteScroll) {
+  //      //console.log('scroll bottom');
+  //      lastAddedLiveFunc();
+  //     }
+  // });
+//******************************************************************
 
 })(jQuery);
